a97f358a9738386d22fa669e3530d73d1fd87b38
Lonng <chris@lonng.org>
2019-03-11 13:39:24 +0800

    fill range info

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 
@@ -12,6 +11,7 @@ import (
 	"github.com/pingcap/parser"
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/parser/format"
+	"github.com/pingcap/parser/mysql"
 	_ "github.com/pingcap/tidb/types/parser_driver"
 )
 
@@ -75,7 +75,8 @@ func restore(ctx *format.RestoreCtx, n *ast.CreateTableStmt) error {
 			if i > 0 {
 				ctx.WritePlain(",")
 				ctx.In.Write([]byte{'\n'})
-				genRange(col, ctx.In)
+				ctx.In.Write([]byte(genRange(col)))
+				ctx.In.Write([]byte{'\n'})
 			}
 			if err := col.Restore(ctx); err != nil {
 				return errors.Annotatef(err, "An error occurred while splicing CreateTableStmt ColumnDef: [%v]", i)
@@ -111,6 +112,58 @@ func restore(ctx *format.RestoreCtx, n *ast.CreateTableStmt) error {
 	return nil
 }
 
-func genRange(col *ast.ColumnDef, writer io.Writer) {
-	writer.Write([]byte("{{ rand.regex('[0-9a-z]{2}') }}\n"))
+func genRange(col *ast.ColumnDef) string {
+	//map[1:1717 2:88 3:4837 4:1 5:33 7:2661 8:3286 10:229 11:11 12:749 15:8319 16:74 246:2149 250:2 251:47 252:241 254:228]
+	switch col.Tp.Tp {
+	case mysql.TypeDecimal:
+		return "{{ rand.range(1, 0xFFFFFFFFFFFFFFFF)}}"
+	case mysql.TypeTiny:
+		return "{{ rand.range(1, 0xFF)}}"
+	case mysql.TypeShort:
+		return "{{ rand.range(1, 0xFFFF)}}"
+	case mysql.TypeLong:
+		return "{{ rand.range(1, 0xFFFFFFFF)}}"
+	case mysql.TypeFloat:
+		return "{{ rand.finite_f32() }}"
+	case mysql.TypeDouble:
+		return "{{ rand.finite_f64() }}"
+	case mysql.TypeNull:
+		return "{{ NULL }}"
+	case mysql.TypeTimestamp:
+		return "{{ rand.u31_timestamp() }}"
+	case mysql.TypeLonglong:
+		return "{{ rand.range(1, 0xFFFFFFFFFFFFFFFF)}}"
+	case mysql.TypeInt24:
+		return "{{ rand.range(1, 0xFFFFFF)}}"
+	case mysql.TypeDate:
+		return "{{ TIMESTAMP '2016-01-02' }}"
+	case mysql.TypeDuration:
+		return "{{ INTERVAL 30 DAY }}"
+	case mysql.TypeDatetime:
+		return "{{ rand.u31_timestamp() }}"
+	case mysql.TypeYear:
+		return "{{ rand.range(1970, 2200)}}"
+	case mysql.TypeNewDate:
+		return "{{ TIMESTAMP '2016-01-02' }}"
+	case mysql.TypeBit:
+		return "{{ rand.range_inclusive(0, 1)}}"
+	case mysql.TypeNewDecimal:
+		return "{{ rand.range(1, 0xFFFFFFFFFFFFFFFF)}}"
+	case mysql.TypeEnum:
+		return unimplemented()
+	case mysql.TypeTinyBlob:
+		return "{{ rand.regex('[0-9a-z]+', 'i', 100) "
+	case mysql.TypeMediumBlob:
+		return "{{ rand.regex('[0-9a-z]+', 'i', 32600) "
+	case mysql.TypeLongBlob:
+		return "{{ rand.regex('[0-9a-z]+', 'i', 1000000) "
+	case mysql.TypeVarchar, mysql.TypeBlob, mysql.TypeVarString, mysql.TypeString:
+		return fmt.Sprintf("{{ rand.regex('[0-9a-z]+', 'i', %d) }}", col.Tp.Flen)
+	default:
+		return unimplemented()
+	}
+}
+
+func unimplemented() string {
+	panic("unimplemented")
 }
